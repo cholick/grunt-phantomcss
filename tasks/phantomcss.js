194cb60e1e12fea824f11e2a4e73376da10b6130
Jon Yonker <jyonker@rallydev.com>
2014-08-08 14:13:47 -0700

    Added SSL support

--- tasks/phantomcss.js
+++ tasks/phantomcss.js
@@ -167,6 +167,7 @@ module.exports = function(grunt) {
         grunt.util.spawn({
             cmd: phantomBinaryPath,
             args: [
+                '--ignore-ssl-errors=true',
                 runnerPath,
                 JSON.stringify(options)
             ],
